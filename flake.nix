dd3a72ca389d676546cf817dd6502b902dd7d89d
Austin Horstman <khaneliman12@gmail.com>
2024-05-07 08:56:04 -0500

    flake.nix: refactor to clean up

--- flake.nix
+++ flake.nix
@@ -9,14 +9,17 @@
     flake = false;
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       # Generate a user-friendly version number.
       version = builtins.substring 0 8 self.lastModifiedDate;
 
       # System types to support.
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -26,51 +29,49 @@
     in
     {
       # Provide some binary packages for selected system types.
-      packages = forAllSystems
-        (system:
-          {
-            sddm-catppuccin = nixpkgsFor.${system}.stdenvNoCC.mkDerivation
-              {
-                pname = "sddm-catppuccin";
-                version = version;
+      packages = forAllSystems (system: {
+        catppuccin-sddm-corners = nixpkgsFor.${system}.stdenvNoCC.mkDerivation {
+          pname = "catppuccin-sddm-corners";
+          inherit version;
 
-                src = nixpkgsFor.${system}.fetchFromGitHub {
-                  owner = "khaneliman";
-                  repo = "sddm-catppuccin";
-                  rev = "7b7a86ee9a5a2905e7e6623d2af5922ce890ef79";
-                  hash = "sha256-sTnt8RarNXz3RmYfmx4rD+nMlY8rr2n0EN3ntPzOurw=";
-                };
+          src = nixpkgs.lib.cleanSourceWith {
+            filter = name: type: type != "regular" || !nixpkgs.lib.hasSuffix ".nix" name;
+            src = nixpkgs.lib.cleanSource ./.;
+          };
 
-                dontConfigure = true;
-                dontBuild = true;
+          dontConfigure = true;
+          dontBuild = true;
 
-                installPhase = ''
-                  runHook preInstall
-                  mkdir -p "$out/share/sddm/themes/"
-                  cp -r catppuccin/ "$out/share/sddm/themes/"
-                  runHook postInstall
-                '';
+          installPhase = ''
+            runHook preInstall
 
-                meta = {
-                  description = "Soothing pastel theme for SDDM";
-                  homepage = "https://github.com/khaneliman/sddm-catppuccin";
-                  license = nixpkgs.lib.licenses.mit;
-                  maintainers = with nixpkgs.lib.maintainers; [ khaneliman ];
-                  platforms = nixpkgs.lib.platforms.linux;
-                };
-              };
-          });
+            mkdir -p "$out/share/sddm/themes/"
+            cp -r catppuccin/ "$out/share/sddm/themes/catppuccin-sddm-corners"
+
+            runHook postInstall
+          '';
+
+          meta = {
+            description = "Soothing pastel theme for SDDM";
+            homepage = "https://github.com/khaneliman/catppuccin-sddm-corners";
+            license = nixpkgs.lib.licenses.mit;
+            maintainers = with nixpkgs.lib.maintainers; [ khaneliman ];
+            platforms = nixpkgs.lib.platforms.linux;
+          };
+        };
+      });
 
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
       # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.sddm-catppuccin);
+      defaultPackage = forAllSystems (system: self.packages.${system}.catppuccin-sddm-corners);
 
-      devShell = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
+      devShell = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
         in
-        pkgs.mkShell {
-          buildInputs = with pkgs; [ ];
-        });
+        pkgs.mkShell { buildInputs = with pkgs; [ ]; }
+      );
     };
 }
